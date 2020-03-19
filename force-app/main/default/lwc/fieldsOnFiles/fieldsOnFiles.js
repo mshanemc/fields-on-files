0047dd243cbeee4f163b860007b4aaf50ea31647
mshanemc <shane.mclaughlin@salesforce.com>
2020-03-19 13:19:54 -0500

    upload component WAD; remove cmdt stuff

--- force-app/main/default/lwc/fieldsOnFiles/fieldsOnFiles.js
+++ force-app/main/default/lwc/fieldsOnFiles/fieldsOnFiles.js
@@ -1,5 +1,5 @@
-import { LightningElement, api, wire, track } from 'lwc';
-// import getSerialized from '@salesforce/apex/fieldsOnFiles.getSerialized';
+import { LightningElement, api, wire } from 'lwc';
+import modifyContentDocumentLink from '@salesforce/apex/fieldsOnFiles.modifyContentDocumentLink';
 import getCVId from '@salesforce/apex/fieldsOnFiles.getCVId';
 
 export default class FieldsOnFiles extends LightningElement {
@@ -7,23 +7,35 @@ export default class FieldsOnFiles extends LightningElement {
     return ['.png', '.gif', 'jpg', '.mov', 'jpeg'];
   }
 
+  _fieldsListArray = [];
+
   // "environment" variables
   @api recordId;
   @api objectApiName;
 
   // configurable properties from design
-  @api fieldsList = [];
   @api showSharing;
   @api showPreview;
+  @api defaultCustomerVisibility;
 
-  @track metadata = {};
-  @track fieldsMetadata = [];
+  @api
+  get fieldsList() {
+    return this._fieldsListArray || [];
+  }
+
+  set fieldsList(configString) {
+    // app builder provides a string, comma separated.  convert into a trimmed array of strings;
+    this._fieldsListArray = configString.split(',').map(item => item.trim());
+  }
 
   latestCDid;
   latestCVid;
+  customerVisibility;
 
   connectedCallback() {
     console.log('fields list: ', this.fieldsList);
+    console.log(`default customer visibility`, this.defaultCustomerVisibility);
+    this.customerVisibility = this.defaultCustomerVisibility;
   }
 
   @wire(getCVId, { ContentDocumentId: '$latestCDid' })
@@ -36,14 +48,32 @@ export default class FieldsOnFiles extends LightningElement {
     }
   }
 
-  handleUploadFinished(event) {
+  async handleUploadFinished(event) {
     // const uploadedFiles = event.detail.files;
     console.log(event.detail.files);
     this.latestCDid = event.detail.files[0].documentId;
+    await this.visibilityUpdate();
+  }
+
+  async visibilityUpdate() {
+    const visible = this.showSharing
+      ? this.template.querySelector(
+          'lightning-input[data-locator="visibility"]'
+        ).checked
+      : this.defaultCustomerVisibility;
+    console.log(`about to set CDL visibility to ${visible}`);
+
+    await modifyContentDocumentLink({
+      LinkedEntityId: this.recordId,
+      ContentDocumentId: this.latestCDid,
+      Visibility: visible ? 'AllUsers' : 'InternalUsers'
+    });
   }
 
   handleCVUpdate() {
+    // reset everything
     this.latestCDid = undefined;
     this.latestCVid = undefined;
+    this.customerVisibility = this.defaultCustomerVisibility;
   }
 }
--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
-sfdx force:user:permset:assign -n FieldsOnFilesPerms
 sfdx automig:load -d data
 sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent
